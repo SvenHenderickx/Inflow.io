2ec084d16bbe085b22d7c724d6c9214f85d09c11
Sven Henderickx <henderickx.sven@gmail.com>
2020-06-12 13:53:01 +0200

    First commit, set up

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>
+            Inflow.io
+        </title>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" type="text/css" href="style/normalize.css" />
+        <link rel="stylesheet" type="text/css" href="style/default.css" />
+
+        <!-- My own styles and script -->
+        <link rel="stylesheet" type="text/css" href="style/style.css">
+        <script src="script/script.js"></script>
+
+        <!-- jQuery & Fonts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <h1>
+
+        </h1>
+
+    </body>
+</html>
