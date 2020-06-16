ff686c3cb868dec8f930c4a2071d95570772a85d
Sven Henderickx <henderickx.sven@gmail.com>
2020-06-16 16:38:20 +0200

    Little edits

--- components/request_detail.js
+++ components/request_detail.js
@@ -1,6 +1,6 @@
 const request_detail = Vue.component('request_detail', {
     props: ['request'],
-    template: '<div><button class="closeButton"  v-on:click="closeDetail()">X</button><input class="inputLikeText title" v-model="request.title"></input><textarea rows="3" cols="40" class="inputLikeText normal" v-model="request.description"></textarea></div>',
+    template: '<div><button class="closeButton"  v-on:click="closeDetail()">X</button><input class="inputLikeText title" v-model="request.title"></input><textarea placeholder="Hier komt de beschrijving van je data aanvraag." rows="3" cols="40" class="inputLikeText normal" v-model="request.description"></textarea><label>Waar wordt de data voor gebruikt?</label><label>Waarom willen we deze data gebruiken?</label></div>',
     computed: {
 
     },
